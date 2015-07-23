f326df9c0d57088225e7576acc56d565c715eb3a
Apprentice <apprentice@StMac2.local>
2015-07-24 11:42:17 +1200

    Database are ready

--- source/Ruby/db/migrate/20150724103031_create_poems.rb
+++ source/Ruby/db/migrate/20150724103031_create_poems.rb
@@ -3,8 +3,10 @@ require_relative '../../config/application'
 class CreatePoems < ActiveRecord::Migration
   def change
      create_table :poems do |t|
-        t.string :description
-        t.boolean :complete
+        t.text :poem_text
+        t.string :title
+        t.string :poet
+        t.string :about
         t.timestamps null: false
      end
   end
