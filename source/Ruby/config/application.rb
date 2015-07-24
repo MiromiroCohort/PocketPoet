117af2e95b56f5321a48a564ddbeefd804f17246
Apprentice <apprentice@StMac2.local>
2015-07-24 14:41:54 +1200

    basics work

--- source/Ruby/config/application.rb
+++ source/Ruby/config/application.rb
@@ -2,6 +2,8 @@ require 'pathname'
 require 'sqlite3'
 require 'active_record'
 require 'logger'
+require_relative '../app/views/view.rb'
+
 
 APP_ROOT = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
 
