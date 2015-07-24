8f4afa4988c71e2e97538ca16c02ddb5c9e5310f
Apprentice <apprentice@StMac2.local>
2015-07-24 12:26:48 +1200

    Pick poem method created

--- source/Ruby/app/models/poem.rb
+++ source/Ruby/app/models/poem.rb
@@ -1,4 +1,10 @@
 require_relative '../../config/application'
 class Poem < ActiveRecord::Base
 
+def self.pick_poem
+  random_poem_num =rand(self.all.count)
+  random_poem =self.all[random_poem_num]
+  return random_poem
+end
+
 end
