a7adfb82367616ce489eeb5dfcd6422c6335a201
Apprentice <apprentice@StMac2.local>
2015-07-24 12:26:57 +1200

    Pick poem method created

---
+++ source/Ruby/app/models/poem_speaker.rb
@@ -0,0 +1,10 @@
+require_relative '../../config/application'
+require 'mactts'
+poem =Poem.pick_poem
+# Poem.all.each do |poem|
+#   p poem.poem_text.split()[0]
+
+#   Mac::TTS.say(poem.poem_text, :alex)
+
+# end
+
